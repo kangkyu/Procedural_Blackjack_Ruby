4d391da87b31b0386862bb505e2ca179e884349f
Kangkyu Lee <lee@kangkyu.com>
2014-04-10 22:36:45 -0700

    add some more feature

--- on_git.rb
+++ on_git.rb
@@ -9,51 +9,91 @@ nums = { :ace => 11,
 	:eight => 8,
 	:nine => 9,
 	:ten => 10,
-	:ja => 10,
-	:qu => 10,
-	:ka => 10 }
+	:jack => 10,
+	:queen => 10,
+	:king => 10 }
 suits = ['spade', 'heart', 'diamond', 'club']
-# a deck ready, for example = [[{:seven=>7}, "spade"], [{:ja=>10}, "club"], [{:qu=>10}, "spade"], [{:qu=>10}, "heart"], [{:ace=>11}, "spade"], [{:nine=>9}, "diamond"], [{:seven=>7}, "club"], [{:six=>6}, "club"], [{:three=>3}, "heart"], [{:six=>6}, "diamond"], [{:five=>5}, "diamond"], [{:four=>4}, "club"], [{:ka=>10}, "diamond"], [{:four=>4}, "spade"], [{:nine=>9}, "heart"], [{:eight=>8}, "spade"], [{:six=>6}, "heart"], [{:eight=>8}, "heart"], [{:nine=>9}, "club"], [{:ja=>10}, "spade"], [{:three=>3}, "spade"], [{:three=>3}, "diamond"], [{:ka=>10}, "spade"], [{:ten=>10}, "club"], [{:ace=>11}, "club"], [{:ka=>10}, "heart"], [{:ka=>10}, "club"], [{:three=>3}, "club"], [{:two=>2}, "club"], [{:five=>5}, "heart"], [{:ja=>10}, "heart"], [{:ten=>10}, "heart"], [{:eight=>8}, "diamond"], [{:eight=>8}, "club"], [{:four=>4}, "heart"], [{:ja=>10}, "diamond"], [{:five=>5}, "club"], [{:two=>2}, "diamond"], [{:qu=>10}, "club"], [{:ace=>11}, "heart"], [{:ten=>10}, "spade"], [{:two=>2}, "spade"], [{:six=>6}, "spade"], [{:ace=>11}, "diamond"], [{:nine=>9}, "spade"], [{:five=>5}, "spade"], [{:two=>2}, "heart"], [{:qu=>10}, "diamond"], [{:seven=>7}, "heart"], [{:four=>4}, "diamond"], [{:ten=>10}, "diamond"], [{:seven=>7}, "diamond"]]
+# a set of cards for example = [[{:seven=>7}, "spade"], [{:ja=>10}, "club"], [{:qu=>10}, "spade"], [{:qu=>10}, "heart"], [{:ace=>11}, "spade"], [{:nine=>9}, "diamond"], [{:seven=>7}, "club"], [{:six=>6}, "club"], [{:three=>3}, "heart"], [{:six=>6}, "diamond"], [{:five=>5}, "diamond"], [{:four=>4}, "club"], [{:ka=>10}, "diamond"], [{:four=>4}, "spade"], [{:nine=>9}, "heart"], [{:eight=>8}, "spade"], [{:six=>6}, "heart"], [{:eight=>8}, "heart"], [{:nine=>9}, "club"], [{:ja=>10}, "spade"], [{:three=>3}, "spade"], [{:three=>3}, "diamond"], [{:ka=>10}, "spade"], [{:ten=>10}, "club"], [{:ace=>11}, "club"], [{:ka=>10}, "heart"], [{:ka=>10}, "club"], [{:three=>3}, "club"], [{:two=>2}, "club"], [{:five=>5}, "heart"], [{:ja=>10}, "heart"], [{:ten=>10}, "heart"], [{:eight=>8}, "diamond"], [{:eight=>8}, "club"], [{:four=>4}, "heart"], [{:ja=>10}, "diamond"], [{:five=>5}, "club"], [{:two=>2}, "diamond"], [{:qu=>10}, "club"], [{:ace=>11}, "heart"], [{:ten=>10}, "spade"], [{:two=>2}, "spade"], [{:six=>6}, "spade"], [{:ace=>11}, "diamond"], [{:nine=>9}, "spade"], [{:five=>5}, "spade"], [{:two=>2}, "heart"], [{:qu=>10}, "diamond"], [{:seven=>7}, "heart"], [{:four=>4}, "diamond"], [{:ten=>10}, "diamond"], [{:seven=>7}, "diamond"]]
 deck = []
-suits.each do |suit|
-	nums.each do |key, value|
-		deck.push [{key => value}, suit]
+puts "Hello, before we start the game,"
+puts "may I have your first name?"
+me_name = gets.chomp.capitalize
+puts "Pleased to meet you, #{me_name}."
+def ask_num question
+	puts question
+	say = gets.chomp.to_i
+	if (say >= 2 && say <= 4)
+			say
+	else
+			ask_num "Supposed to say a number between 2 to 4.. how many?"
 	end
 end
+num_deck = ask_num "We use multiple decks, please choose 2 to 4."
+puts "We use #{num_deck} card sets. Let's start in a moment..."
+# multi-deck cards are ready
+(1..num_deck).each do
+	suits.each do |suit|
+		nums.each do |key, value|
+			deck.push [{key => value}, suit]
+		end
+	end
+end
+me_sum = dealer_sum = 0
+puts '=========================='
+puts 'deck size'.ljust(12) + (' = ' + deck.size.to_s).rjust(3)
+puts 'my number'.ljust(12) + (' = ' + me_sum.to_s).rjust(3)
+puts 'dealer\'s'.ljust(12) + (' = ' + dealer_sum.to_s).rjust(3)
+puts '=========================='
+# shuffle and start
+puts 
 deck = deck.sort_by{rand} 
-# method: ask player to decide
-def ask question
+# method: deal_a_card
+def deal_a_card curr_deck
+	card = curr_deck.pop
+	[card, curr_deck]
+end
+# deal two cards for each person
+2.times do
+	after_deal = deal_a_card deck
+	puts "#{me_name}, you've got #{after_deal[0][0].keys.join} of #{after_deal[0][1]}s"
+	me_sum = me_sum + after_deal[0][0].values.join.to_i
+end
+puts "now for you it's #{me_sum}."
+2.times do
+	after_deal = deal_a_card deck 
+	puts "I've got #{after_deal[0][0].keys.join} of #{after_deal[0][1]}s"
+	dealer_sum = dealer_sum + after_deal[0][0].values.join.to_i
+end
+puts "now for me it's #{dealer_sum}."
+puts '=========================='
+puts 'deck size'.ljust(12) + (' = ' + deck.size.to_s).rjust(3)
+puts 'my number'.ljust(12) + (' = ' + me_sum.to_s).rjust(3)
+puts 'dealer\'s'.ljust(12) + (' = ' + dealer_sum.to_s).rjust(3)
+puts '=========================='
+# method: ask player to decide if hit
+def ask_hit question
 	puts question
 	say = gets.chomp.downcase
 	if (say == 'hit' || say == 'stay')
 			say
 	else
-			puts "You are supposed to say 'hit' or 'stay'"
-			ask question
+			ask_hit "#{me_name}, you are supposed to say 'hit' or 'stay'"
 	end
 end
-# method: deal_with_hit - give a card when 'hit'
-def deal_with_hit before_deck
-	curr_deck = before_deck
-	card = curr_deck.pop
-	[card, curr_deck]
-end
-# shuffle and start
-deck = deck.sort_by{rand} 
-me_sum = dealer_sum = 0
-me_closed = dealer_closed = false
+# rally starts
 the_rally = true
 busted = false
+me_closed = dealer_closed = false
 while the_rally
-	# my turn - dealer asks and I decide
+	# my turn - dealer asks, I decide if hit
 	# one more card in my hand if I type 'hit'
 	unless me_closed
-		decision = ask 'Your turn!'
+		decision = ask_hit 'Your turn!'
 		puts "Okay you said '#{decision}'"
 		if decision == 'hit'
-			after_hit = deal_with_hit deck 
-			puts "You've got #{after_hit[0][0].keys.join} of #{after_hit[0][1]}s"
-			me_sum = me_sum + after_hit[0][0].values.join.to_i
+			after_deal = deal_a_card deck 
+			puts "You've got #{after_deal[0][0].keys.join} of #{after_deal[0][1]}s"
+			me_sum = me_sum + after_deal[0][0].values.join.to_i
 			puts "now for you it's #{me_sum}."
 		else
 			puts "Your final number is #{me_sum}."
@@ -77,9 +117,9 @@ while the_rally
 		end
 		if decision == 'hit'
 			puts "I say 'hit'"
-			after_hit = deal_with_hit deck 
-			puts "I've got #{after_hit[0][0].keys.join} of #{after_hit[0][1]}s"
-			dealer_sum = dealer_sum + after_hit[0][0].values.join.to_i
+			after_deal = deal_a_card deck 
+			puts "I've got #{after_deal[0][0].keys.join} of #{after_deal[0][1]}s"
+			dealer_sum = dealer_sum + after_deal[0][0].values.join.to_i
 			puts "now for me it's #{dealer_sum}."
 		else
 			puts "I say 'stay', my final number is #{dealer_sum}"
@@ -96,13 +136,13 @@ while the_rally
 	# in that case, compare number = same('push?') or more number wins
 	if (dealer_closed == true && me_closed == true)
 		if dealer_sum == me_sum
-			puts "we draw. another game?"
+			puts "We draw. another game?"
 		elsif dealer_sum > me_sum
-			puts "your final number is lower, I won."
+			puts "Your final number is lower, I won."
 			puts "And I am BLACKJACK!" if dealer_sum == 21
 		else
-			puts "your final number is higher, You won."
-			puts "And you're BLACKJACK!" if me_sum == 21
+			puts "Your final number is higher, You won."
+			puts "And #{me_name}, you're BLACKJACK!" if me_sum == 21
 		end
 		break
 	end
@@ -112,3 +152,8 @@ while the_rally
 	puts 'dealer\'s'.ljust(12) + (' = ' + dealer_sum.to_s).rjust(3)
 	puts '=========================='
 end
+
+# more to do..
+# Ace value 2 different interpretation
+# calculate-total method
+# ask if the player wants to play again, after a game
